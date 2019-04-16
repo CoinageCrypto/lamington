c3132e152e6382fc14364cfb86d1f93bbe3f42ee
Kevin Brown <kevin.brown@coina.ge>
2019-04-16 17:24:21 +1000

    Build command now acts like test command but without the testing.

--- src/cli/lamington-build.ts
+++ src/cli/lamington-build.ts
@@ -1,5 +1,33 @@
-import { buildAll } from './utils';
+import { eosIsReady, startEos, runTests, stopContainer, buildAll } from './utils';
+import { GitIgnoreManager } from '../gitignoreManager';
+import { ConfigManager } from '../configManager';
 
-buildAll().catch(error => {
-	throw error;
+const run = async () => {
+	if (await eosIsReady()) {
+		console.log('EOS is running. Stopping...');
+		await stopContainer();
+	}
+
+	// This initialises the config
+	console.log('Getting configuration...');
+	await ConfigManager.initWithDefaults();
+
+	// This ensures we have our .gitignore inside the .lamington directory
+	await GitIgnoreManager.createIfMissing();
+
+	console.log('Starting EOS...');
+	await startEos();
+
+	console.log('Building smart contracts...');
+	await buildAll();
+
+	console.log('Stopping EOS...');
+	await stopContainer();
+};
+
+run().catch(error => {
+	console.log('Stopping EOS...');
+	stopContainer().then(() => {
+		throw error;
+	});
 });
