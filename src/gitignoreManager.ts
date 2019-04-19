4422d7b4f33bbaefa386d00ee1381e0b6e6ab387
Mitch Pierias <mitch@pierias.com>
2019-04-19 15:14:00 +1000

    Added doc description

--- src/gitignoreManager.ts
+++ src/gitignoreManager.ts
@@ -5,10 +5,15 @@ import { promisify } from 'util';
 const exists = promisify(existsCallback);
 const writeFile = promisify(writeFileCallback);
 
+/* Defaults */
 const configDirectory = '.lamington';
 const gitignoreFilePath = path.join(configDirectory, '.gitignore');
 const encoding = 'utf8';
 
+/**
+ * @class GitIgnoreManager
+ * @desc Automatically creates and configures a `.gitignore` file for generated files
+ */
 export class GitIgnoreManager {
 	public static async createIfMissing() {
 		if (!(await exists(gitignoreFilePath))) {
