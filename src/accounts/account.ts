7fb1077f4065a2cf3d292fa8e1e66f8e06aba0ee
Kevin Brown <kevin.brown@coina.ge>
2019-04-23 19:37:41 +1000

    Added attempt to get `eosio.code` permission adding working.

--- src/accounts/account.ts
+++ src/accounts/account.ts
@@ -1,5 +1,7 @@
 import { Permissions } from './permissions';
 import * as ecc from 'eosjs-ecc';
+import { Contract } from '../contracts';
+import { AccountManager } from './accountManager';
 
 export class Account {
 	public name: string;
@@ -41,4 +43,8 @@ export class Account {
 			},
 		];
 	}
+
+	public addCodePermission = async (contract: Contract) => {
+		await AccountManager.addCodePermission(this, contract);
+	};
 }
