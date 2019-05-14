2ce5bbbc4107c01e4d946ea766266171dd8a384b
Kevin Brown <kevin.brown@coina.ge>
2019-05-14 10:39:20 +1000

    Added missing package.json check. Let prettier run on the file.

--- src/project/projectManager.ts
+++ src/project/projectManager.ts
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import * as colors from 'colors';
 import * as path from 'path';
 import * as mkdirpCallback from 'mkdirp';
 import {
 	readFile as readFileCallback,
 	writeFile as writeFileCallback,
-    exists as existsCallback,
-    readdir as readdirCallback,
+	exists as existsCallback,
+	readdir as readdirCallback,
 } from 'fs';
 import { promisify } from 'util';
-import { ConfigManager, LamingtonConfig } from "./../configManager";
+import { ConfigManager, LamingtonConfig } from './../configManager';
 import * as spinner from './../cli/logIndicator';
 import { sleep } from '../utils';
 
@@ -26,97 +26,113 @@ const ENCODING = 'utf8';
  * @author Mitch Pierias <github.com/MitchPierias>
  */
 export class ProjectManager {
-
-    /** @hidden Reference to the local `package.json` file */
-    private static pkg:{
-        scripts?: { [key:string]:string },
-        devDependencies?: { [key:string]:string }
-    };
+	/** @hidden Reference to the local `package.json` file */
+	private static pkg: {
+		scripts?: { [key: string]: string };
+		devDependencies?: { [key: string]: string };
+	};
 
 	/**
 	 * Downloads the example project and integrates aspects where required.
 	 * @author Mitch Pierias <github.com/MitchPierias>
 	 */
 	public static async initWithDefaults() {
-        
-        ProjectManager.pkg = JSON.parse(await readFile(path.join(process.cwd(), 'package.json'), ENCODING));
-        
-        await ProjectManager.configureScripts();
+		let packageJson = '';
+		try {
+			packageJson = await readFile(path.join(process.cwd(), 'package.json'), ENCODING);
+		} catch (error) {
+			console.error();
+			console.error(
+				colors.red(
+					'Could not read project package.json file in this folder. Is this a node project folder?'
+				)
+			);
+			console.error();
 
-        await ProjectManager.configureDependencies();
-        // Load Configuration
-        await ConfigManager.initWithDefaults();
+			process.exit(1);
+		}
 
-        await ProjectManager.createProjectStructure();
+		ProjectManager.pkg = JSON.parse(packageJson);
 
-        await ProjectManager.pullExampleProject();
+		await ProjectManager.configureScripts();
 
-        await writeFile(path.join(process.cwd(),'package.json'), JSON.stringify(ProjectManager.pkg, null, 4), ENCODING);
-    }
+		await ProjectManager.configureDependencies();
+		// Load Configuration
+		await ConfigManager.initWithDefaults();
 
-    /**
-     * Configures the projects `package.json` scripts for a standard Lamington environment
-     * @note Should merge the scripts provided in the example project with existing
-     * @author Mitch Pierias <github.com/MitchPierias>
-     * @hidden
-     */
-    private static async configureScripts() {
-        spinner.create('Adding scripts');
-        const { scripts } = ProjectManager.pkg;
-        const defaultScripts = {
-            'build':'lamington build',
-            'test':'lamington test'
-        }
-        ProjectManager.pkg.scripts = { ...defaultScripts, ...scripts };
-        spinner.end('Added scripts');
-    }
+		await ProjectManager.createProjectStructure();
 
-    private static async configureDependencies() {
-        spinner.create('Adding dependencies');
-        const { devDependencies } = ProjectManager.pkg;
-        const defaultDependencies = {
-            'lamington':'latest'
-        }
-        ProjectManager.pkg.devDependencies = { ...defaultDependencies, ...devDependencies };
-        spinner.end('Added recommended dependencies');
-    }
+		await ProjectManager.pullExampleProject();
 
-    /**
-     * Downloads the latest example Lamington project from GitHub
-     * @author Mitch Pierias <github.com/MitchPierias>
-     * @hidden
-     */
-    private static async pullExampleProject() {
-        const files = await readdir(path.join(process.cwd(), 'contracts'));
-        spinner.create('Pulling example project');
-        
-        await sleep(1000);
-        if (files.length <= 0) {
-            spinner.end('Included example project');
-        } else {
-            spinner.end('Project contained contracts');
-        }
-    }
-    
-    /**
-     * Creates the standard directories and files for a Lamington project
-     * @author Mitch Pierias <github.com/MitchPierias>
-     * @hidden
-     */
-    private static async createProjectStructure() {
-        
-        spinner.create('Creating directory structure');
+		await writeFile(
+			path.join(process.cwd(), 'package.json'),
+			JSON.stringify(ProjectManager.pkg, null, 4),
+			ENCODING
+		);
+	}
 
-        await ProjectManager.createDirectoryIfMissing('contracts');
+	/**
+	 * Configures the projects `package.json` scripts for a standard Lamington environment
+	 * @note Should merge the scripts provided in the example project with existing
+	 * @author Mitch Pierias <github.com/MitchPierias>
+	 * @hidden
+	 */
+	private static async configureScripts() {
+		spinner.create('Adding scripts');
+		const { scripts } = ProjectManager.pkg;
+		const defaultScripts = {
+			build: 'lamington build',
+			test: 'lamington test',
+		};
+		ProjectManager.pkg.scripts = { ...defaultScripts, ...scripts };
+		spinner.end('Added scripts');
+	}
 
-        await ProjectManager.createDirectoryIfMissing('.lamington');
+	private static async configureDependencies() {
+		spinner.create('Adding dependencies');
+		const { devDependencies } = ProjectManager.pkg;
+		const defaultDependencies = {
+			lamington: 'latest',
+		};
+		ProjectManager.pkg.devDependencies = { ...defaultDependencies, ...devDependencies };
+		spinner.end('Added recommended dependencies');
+	}
 
-        spinner.end('Created directory structure');
-    }
+	/**
+	 * Downloads the latest example Lamington project from GitHub
+	 * @author Mitch Pierias <github.com/MitchPierias>
+	 * @hidden
+	 */
+	private static async pullExampleProject() {
+		const files = await readdir(path.join(process.cwd(), 'contracts'));
+		spinner.create('Pulling example project');
 
-    private static async createDirectoryIfMissing(dirName:string) {
-        if (!await exists(path.join(process.cwd(), dirName))) {
-            await mkdirp(path.join(process.cwd(), dirName));
-        }
-    }
+		await sleep(1000);
+		if (files.length <= 0) {
+			spinner.end('Included example project');
+		} else {
+			spinner.end('Project contained contracts');
+		}
+	}
+
+	/**
+	 * Creates the standard directories and files for a Lamington project
+	 * @author Mitch Pierias <github.com/MitchPierias>
+	 * @hidden
+	 */
+	private static async createProjectStructure() {
+		spinner.create('Creating directory structure');
+
+		await ProjectManager.createDirectoryIfMissing('contracts');
+
+		await ProjectManager.createDirectoryIfMissing('.lamington');
+
+		spinner.end('Created directory structure');
+	}
+
+	private static async createDirectoryIfMissing(dirName: string) {
+		if (!(await exists(path.join(process.cwd(), dirName)))) {
+			await mkdirp(path.join(process.cwd(), dirName));
+		}
+	}
 }
