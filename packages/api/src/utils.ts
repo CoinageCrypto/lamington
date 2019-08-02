f3106a466a378a58dca331bd71b5b6feab1fcf7c
Mitch Pierias <mitch@pierias.com>
2019-08-02 15:35:04 +1000

    Implemented simple EOS error output message check

--- packages/api/src/utils.ts
+++ packages/api/src/utils.ts
@@ -112,12 +112,12 @@ export const assertRowCount = async (
  * @author Kevin Brown <github.com/thekevinbrown>
  * @param operation Operation promise
  * @param eosErrorName Expected EOS error name
- * @param description Output message description
+ * @param withMessage (optional) Expected exception output message
  */
 export const assertEOSError = async (
 	operation: Promise<any>,
 	eosErrorName: string,
-	description: string
+	withMessage?: string
 ) => {
 	// Execute operation and handle exceptions
 	try {
@@ -129,6 +129,12 @@ export const assertEOSError = async (
 				error.json.error.name === eosErrorName,
 				`Expected ${eosErrorName}, got ${error.json.error.name} instead.`
 			);
+			// Check the error details contain the optional message
+			if (withMessage && error.json.error.details.length > 0) {
+				assert.deepEqual(error.json.error.details[0], {
+					message: `assertion failure with message: ${withMessage}`
+				});
+			}
 			return;
 		} else {
 			// Fail if error not thrown by EOS
@@ -138,16 +144,18 @@ export const assertEOSError = async (
 		}
 	}
 	// Fail if no exception thrown
-	assert.fail(`Expected ${description} but operation completed successfully.`);
+	assert.fail(`Expected ${withMessage || 'error'} but operation completed successfully.`);
 };
 
 /**
  * Asserts operation throws an `eosio_assert_message_exception` error
  * @author Kevin Brown <github.com/thekevinbrown>
+ * @author Mitch Pierias <github.com/MitchPierias>
  * @param operation Operation promise
+ * @param message (optional) Expected exception message
  */
-export const assertEOSException = (operation: Promise<any>) =>
-	assertEOSError(operation, 'eosio_assert_message_exception', 'assert');
+export const assertEOSException = (operation: Promise<any>, message?: string) =>
+	assertEOSError(operation, 'eosio_assert_message_exception', message || 'assert');
 
 /**
  * Asserts operation is missing the required authority by throwing a `missing_auth_exception` error
