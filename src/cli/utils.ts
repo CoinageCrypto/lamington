bd2222087d61a71268182be18bf4f65caab1125a
Syed Jafri <syed.jafri2@ucalgary.ca>
2019-12-11 12:57:04 -0700

    Cover RC

--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -48,7 +48,7 @@ const MAX_CONNECTION_ATTEMPTS = 8;
  */
 const versionFromUrl = (url: string) => {
 	// Looks for strings in this format: `/v1.4.6/`
-	const pattern = /\/(v\d+\.\d+\.\d+)\//g;
+	const pattern = /\/(v\d+\.\d+\.\d+.*)\//g;
 	const result = pattern.exec(url);
 
 	// Handle result
